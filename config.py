86dc2228aa06d444373ba6227671f45b78889ddc
Rubén Cabrera Moya <rcabmoy622@g.educaand.es>
2025-01-04 21:29:46 +0100

    I update files

--- config.py
+++ config.py
@@ -3,6 +3,5 @@ import os
 SECRET_KEY = '1'
 PWD = os.path.abspath(os.curdir)
 
-DEBUG = True
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}/bbdd/dbase.db'.format(PWD)
 SQLALCHEMY_TRACK_MODIFICATIONS = False
--- manage.py
+++ manage.py
@@ -7,7 +7,6 @@ app = Flask(__name__)
 app.config.from_object(config)
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
-app.config['DEBUG'] = True
 
 def create_tables():
     "Create relational database tables"
