94886c3cf438203fd4167a901ecb330d35e796ba
Rubén <rcabmoy622@g.educaand.es>
2025-02-04 13:21:54 +0100

    I update files

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 SECRET_KEY = '1'
 PWD = os.path.abspath(os.curdir)
 
-SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://ruben:1@localhost/library_mysql'
+SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://ruben:1@localhost/mysql_library'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
